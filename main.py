ebba559d64f853f0016ae84d9eb8a17c16dd6427
Keyvan Sadeghi <ksa128@uit.no>
2024-04-02 14:49:42 +0200

    created main function for running program

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-import Q_learning
+from Q_learning import *
+from SARSA_learning import *
+
+if __name__ == "__main__":
+    Q_model = Q_agent()
+    Q_model.train_model()
+
+    SARSA_model = SARSA_Agent()
+    SARSA_model.train_model()
